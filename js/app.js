5e02d0beb09e5f0a9c8f9eb90e4db30448035427
Zayah117 <isaiah.baker@simpledove.com>
2016-06-30 17:05:26 -0400

    Change where cowboys start

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var Cowboy = function(x, y, sprite) {
 	this.x = x;
-	this.y = x;
+	this.y = y;
 	this.sprite = sprite;
 	this.movingUp = false;
 	this.movingDown = false;
@@ -47,8 +47,8 @@ Cowboy.prototype.keyDown = function(key) {
 	}
 };
 
-goodGuy = new Cowboy(50, 50, 'images/good-guy.png');
-badGuy = new Cowboy(100, 50, 'images/bad-guy.png');
+goodGuy = new Cowboy(30, 250, 'images/good-guy.png');
+badGuy = new Cowboy(500, 250, 'images/bad-guy.png');
 
 document.addEventListener('keydown', function(e) {
 	var player1Keys = {
