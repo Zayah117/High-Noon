d3cce98ba59cbb352b8936a85da5c7201dd97c96
Zayah117 <isaiah.baker@simpledove.com>
2016-06-30 16:54:33 -0400

    Make cowboys move smoothly

--- js/app.js
+++ js/app.js
@@ -2,29 +2,50 @@ var Cowboy = function(x, y, sprite) {
 	this.x = x;
 	this.y = x;
 	this.sprite = sprite;
+	this.movingUp = false;
+	this.movingDown = false;
 };
 
 Cowboy.prototype.update = function(dt) {
-	// TODO
+	if (this.movingUp == true) {
+		this.y -= 3;
+	}
+	if (this.movingDown == true) {
+		this.y += 3;
+	}
 };
 
 Cowboy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Cowboy.prototype.handleInput = function(key) {
+Cowboy.prototype.keyUp = function(key) {
 	upKeys = ['w', 'up'];
 	downKeys = ['s', 'down'];
 	function isInArray(value, array) {
 		return array.indexOf(value) > -1;
 	}
 	if (isInArray(key, upKeys)) {
-		this.y -= 3;
+		this.movingUp = false;
 	}
 	if (isInArray(key, downKeys)) {
-		this.y += 3;
+		this.movingDown = false;
 	}
-}
+};
+
+Cowboy.prototype.keyDown = function(key) {
+	upKeys = ['w', 'up'];
+	downKeys = ['s', 'down'];
+	function isInArray(value, array) {
+		return array.indexOf(value) > -1;
+	}
+	if (isInArray(key, upKeys)) {
+		this.movingUp = true;
+	}
+	if (isInArray(key, downKeys)) {
+		this.movingDown = true;
+	}
+};
 
 goodGuy = new Cowboy(50, 50, 'images/good-guy.png');
 badGuy = new Cowboy(100, 50, 'images/bad-guy.png');
@@ -39,10 +60,27 @@ document.addEventListener('keydown', function(e) {
 		40: 'down'
 	};
 	if (player1Keys.hasOwnProperty(e.keyCode)) {
-		goodGuy.handleInput(player1Keys[e.keyCode]);
+		goodGuy.keyDown(player1Keys[e.keyCode]);
 	}
 	if (player2Keys.hasOwnProperty(e.keyCode)) {
-		badGuy.handleInput(player2Keys[e.keyCode]);
+		badGuy.keyDown(player2Keys[e.keyCode]);
+	}
+});
+
+document.addEventListener('keyup', function(e) {
+	var player1Keys = {
+		87: 'w',
+		83: 's'
+	};
+	var player2Keys = {
+		38: 'up',
+		40: 'down'
+	};
+	if (player1Keys.hasOwnProperty(e.keyCode)) {
+		goodGuy.keyUp(player1Keys[e.keyCode]);
+	}
+	if (player2Keys.hasOwnProperty(e.keyCode)) {
+		badGuy.keyUp(player2Keys[e.keyCode]);
 	}
 });
 
